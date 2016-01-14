06396815c7fe99045e5fb0be986e716684dbbecf
THIEN PHAM <thien@tinypulse.com>
2016-01-14 20:47:32 +0700

    locale

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,7 +1,19 @@
 class LanguageBot < SlackRubyBot::Bot
   command 'hello' do |client, data, match|
-  	I18n.locale = :vi
+
+  	user = User.find_or_create_by(username: data.user)
+
+  	I18n.locale = user.locale
   	text = I18n.t "hello", user: data.user
   	client.say(text: text, channel: data.channel)
   end
+
+  command 'locale' do |client, data, match|
+  	user = User.find_or_create_by(username: data.user)
+	user.locale = match[:expression]
+	user.save
+	client.say(text: "The lang language now is #{user.locale}", channel: data.channel)
+
+  end
+
 end
