11e9a5bfbc547ed7474cea5b889e2766e10f267b
Thuan Bui <thubui@atlassian.com>
2016-01-14 20:16:26 +0700

    milestone 2

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,3 +1,7 @@
 class LanguageBot < SlackRubyBot::Bot
-  # leave it empty for now
+  command 'hello' do |client, data, match|
+  	I18n.locale = :vi
+  	text = I18n.t "hello", user: data.user
+  	client.say(text: text, channel: data.channel)
+  end
 end
